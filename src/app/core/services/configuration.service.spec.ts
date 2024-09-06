0e50402d82405d395803a7a0293e4c7d2a1b4118
Rong Chen <rjunchen@gmail.com>
2024-09-06 01:59:20 -0700

    fix unit test

--- src/app/core/services/configuration.service.spec.ts
+++ src/app/core/services/configuration.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ConfigurationService } from './configuration.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('ConfigurationService', () => {
   let service: ConfigurationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClientTesting(), provideHttpClient()]
+    });
     service = TestBed.inject(ConfigurationService);
   });
 
